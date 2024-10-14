0b1a8e73b47dc32376fc227df22adc74635e773b
diegolopez-dev <diego.teide@gmail.com>
2024-10-15 01:23:21 +0200

    config consumer

--- SpringBootProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
+++ SpringBootProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
@@ -16,11 +16,19 @@ public class KafkaTopicConfig {
     public NewTopic generateTopic(){
 
         Map<String, String> configurations = new HashMap<>();
+        //Borrar el mensaje despues de cierto tiempo
         configurations.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE);
+        //Tiempo de retencion del mensaje
+        configurations.put(TopicConfig.RETENTION_MS_CONFIG, "86400000");
+        //Tamaño del segmento
+        configurations.put(TopicConfig.SEGMENT_BYTES_CONFIG, "1073741824");
+        //Tamaño maximo de cada mensaje
+        configurations.put(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, "100012");
 
-        return TopicBuilder.name("diegoLópezTopic")
+        return TopicBuilder.name("diegoLopezTopic")
                 .partitions(2)
                 .replicas(2)
+                .configs(configurations)
                 .build();
     }
 }
