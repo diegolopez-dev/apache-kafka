42ec90a3419d75a305214f8d9d8ac78ae775944b
diegolopez-dev <diego.teide@gmail.com>
2024-10-14 23:49:51 +0200

    config provider

---
+++ SpringBootProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
@@ -0,0 +1,26 @@
+package com.kafka.provider.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic generateTopic(){
+
+        Map<String, String> configurations = new HashMap<>();
+        configurations.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE);
+
+        return TopicBuilder.name("diegoLópezTopic")
+                .partitions(2)
+                .replicas(2)
+                .build();
+    }
+}
